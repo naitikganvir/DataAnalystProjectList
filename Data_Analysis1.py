be8f7f7a40a45af9f864ec34ab7cd383908de1e4
Naitik Ganvir <135999483+naitikganvir@users.noreply.github.com>
2025-11-11 12:40:49 +0530

    Add files via upload

---
+++ Data_Analysis1.py
@@ -0,0 +1,15 @@
+print("Hello I m a Data Analyst")
+
+print("Work of data analyst collect, clean, visualize and give insights from the data")
+
+#Role : Raw data to Useful Insights for Business, Government and Organization
+
+#Types of Data
+# Structured , Unstructured, Semi Structured Data
+
+#Types of Data Analysis
+
+print("DDPP, D for Descriptive analysis which is tell What did happened"
+      "D for Diagnostic Analysis which is tell Why this is Happen"
+      "P for Predictive Analysis which is tell What will happen"
+      "P for Prescriptive Analysis which is tell What should to do, Recommendations")
---
+++ Data_Analysis2.py
@@ -0,0 +1,10 @@
+#Python Variables
+
+x = 20
+print("My age is ",x)
+
+a, b, c = "Orange", "Banana", "Cherry"
+
+print(a)
+print(b)
+print(c)
